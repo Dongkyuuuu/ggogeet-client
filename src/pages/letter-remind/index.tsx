a49749f9226660aefb8f368ade2688c21772fc75
KimGaeun <greencloud11@hanyang.ac.kr>
2022-12-27 10:07:43 +0900

    🚧 꼬깃 기억 페이지 헤더 UI 작업 중

--- src/pages/letter-remind/index.tsx
+++ src/pages/letter-remind/index.tsx
@@ -1,11 +1,11 @@
 import Button from "@/src/components/common/Buttons/Button";
 import TopNavigation from "@/src/components/common/TopNavigation";
 import { NavBack } from "@/src/components/common/TopNavigation/atoms";
-import TodoContainer from "@/src/components/features/letter-remind/TodoContainer";
+import RemindNavigationBar from "@/src/components/features/letter-remind/Main/RemindNavigationBar";
+import TodoContainer from "@/src/components/features/letter-remind/Main/TodoContainer";
 import { RemindData } from "@/src/data/LetterRemind";
 import { Display2 } from "@/src/styles/commons";
 import styled from "@emotion/styled";
-import Image from "next/image";
 
 const Layout = styled.div`
   background-color: ${({ theme }) => theme.colors.navy};
@@ -41,6 +41,7 @@ const LetterRemindPage = () => {
       />
 
       <MainLayout>
+        <RemindNavigationBar />
         {/* <ButtonsContainer>
           <Button name="모든 꼬깃기억" fontStyle="bold" isDark={true} />
           <Button name="미완료" fontStyle="bold" isDark={true} />
