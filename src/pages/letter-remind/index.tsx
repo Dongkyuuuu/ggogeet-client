e0c38867d62a06c03846baae1be06c74ced36f73
KimGaeun <greencloud11@hanyang.ac.kr>
2022-12-27 10:33:09 +0900

    💄 TodoContainerList 컴포넌트 생성

--- src/pages/letter-remind/index.tsx
+++ src/pages/letter-remind/index.tsx
@@ -3,6 +3,7 @@ import TopNavigation from "@/src/components/common/TopNavigation";
 import { NavBack } from "@/src/components/common/TopNavigation/atoms";
 import RemindNavigationBar from "@/src/components/features/letter-remind/Main/RemindNavigationBar";
 import TodoContainer from "@/src/components/features/letter-remind/Main/TodoContainer";
+import TodoContainerList from "@/src/components/features/letter-remind/Main/TodoContainerList";
 import { RemindData } from "@/src/data/LetterRemind";
 import { Display2 } from "@/src/styles/commons";
 import styled from "@emotion/styled";
@@ -16,11 +17,8 @@ const MainLayout = styled.div`
   padding: 12px 16px 0 16px;
 `;
 
-const ButtonsContainer = styled.div`
-  display: flex;
-  width: 270px;
-  padding: 0 0 28px 0;
-  justify-content: space-between;
+const TodoLayout = styled.div`
+  margin: 24px 0 0 0;
 `;
 
 const TodoContainerWrapper = styled.div`
@@ -36,25 +34,16 @@ const LetterRemindPage = () => {
   return (
     <Layout>
       <TopNavigation
-        title={<TopNavigationTitle>꼬깃 기억</TopNavigationTitle>}
+        title={<TopNavigationTitle>꼬깃 메모</TopNavigationTitle>}
         leftElem={<NavBack color="white" />}
       />
 
       <MainLayout>
         <RemindNavigationBar selectedItem="모든 메모" />
-        {/* <ButtonsContainer>
-          <Button name="모든 꼬깃기억" fontStyle="bold" isDark={true} />
-          <Button name="미완료" fontStyle="bold" isDark={true} />
-          <Button name="완료" fontStyle="bold" isDark={true} />
-        </ButtonsContainer> */}
 
-        {RemindData.map((item) => {
-          return (
-            <TodoContainerWrapper key={item.id}>
-              <TodoContainer todo={item} />
-            </TodoContainerWrapper>
-          );
-        })}
+        <TodoLayout>
+          <TodoContainerList />
+        </TodoLayout>
       </MainLayout>
     </Layout>
   );
